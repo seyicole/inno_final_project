f07ec4e77aec848049b0f2fd3cc89194c3e02961
Oluseyi Imanah <seyicole@gmail.com>
2022-07-13 15:49:43 +0100

    added 5 pages and structure

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+from flask import Flask, render_template, redirect, url_for
+from views import my_view
 
+app = Flask(__name__)
+app.register_blueprint(my_view)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# @app.route('/javascript')
+# def red():
+#     return redirect(url_for('script'))
+
+# @app.route('/script')
+# def script():
+#     return render_template('index.html')
+
+# @app.route('/js')
+# def blue():
+#     return redirect(url_for('jsfile'))
+
+# @app.route('/jsfile')
+# def jsfile():
+#     return render_template('index.html')
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True, port=8000)
---
+++ views.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, Blueprint, redirect, url_for
+
+my_view = Blueprint('my_view', __name__)
+
+@my_view.route('/')
+def index():
+    return render_template('index.html')
+
+@my_view.route('/about')
+def about():
+    return render_template('about.html')
+
+@my_view.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@my_view.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@my_view.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+# @app.route('/page5')
+# def page5():
+#     return render_template('page5.html')
+
+@my_view.route('/admin')
+def admin():
+    return render_template('admin.html')
+
